9f81041dc7d713ebc3b5802f943b43c19bf1eb7b
Martin Michelsen <fuzziqersoftware+g@gmail.com>
2022-06-26 22:59:14 -0700

    add accidentally-omitted file

---
+++ src/Loggers.hh
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <phosg/Strings.hh>
+#include <phosg/JSON.hh>
+
+
+
+extern PrefixedLogger ax_messages_log;
+extern PrefixedLogger channel_exceptions_log;
+extern PrefixedLogger client_log;
+extern PrefixedLogger command_data_log;
+extern PrefixedLogger config_log;
+extern PrefixedLogger dns_server_log;
+extern PrefixedLogger function_compiler_log;
+extern PrefixedLogger ip_stack_simulator_log;
+extern PrefixedLogger license_log;
+extern PrefixedLogger lobby_log;
+extern PrefixedLogger player_data_log;
+extern PrefixedLogger proxy_server_log;
+extern PrefixedLogger server_log;
+extern PrefixedLogger static_game_data_log;
+
+void set_log_levels_from_json(std::shared_ptr<JSONObject> json);
