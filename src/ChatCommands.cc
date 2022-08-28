34b70a8a03932ccbc1432156301cad40944f32fc
Martin Michelsen <fuzziqersoftware+g@gmail.com>
2022-08-27 18:58:06 -0700

    don't send event change command to GCTE clients

--- src/ChatCommands.cc
+++ src/ChatCommands.cc
@@ -253,9 +253,10 @@ static void proxy_command_lobby_event(shared_ptr<ServerState>,
       send_text_message(session.client_channel, u"$C6No such lobby event.");
     } else {
       session.override_lobby_event = new_event;
-      if ((session.version == GameVersion::GC) ||
-          (session.version == GameVersion::XB) ||
-          (session.version == GameVersion::BB)) {
+      if (((session.version == GameVersion::GC) ||
+           (session.version == GameVersion::XB) ||
+           (session.version == GameVersion::BB)) &&
+           !(session.newserv_client_config.cfg.flags & Client::Flag::GC_TRIAL_EDITION)) {
         session.client_channel.send(0xDA, session.override_lobby_event);
       }
     }
