8f2bf60d62dfa27c896eb19e0a9e29c220fef65d
Martin Michelsen <fuzziqersoftware+g@gmail.com>
2022-09-26 15:30:56 -0700

    add UDP off versions of v2 login command

--- src/SendCommands.cc
+++ src/SendCommands.cc
@@ -33,7 +33,9 @@ const unordered_set<uint32_t> v2_crypt_initial_client_commands({
   0x01140093, // (02) DCv1 extended login
   0x00E0009A, // (17) DCv2 license check
   0x00CC009D, // (02) DCv2 login
+  0x00CC019D, // (02) DCv2 login (UDP off)
   0x0130009D, // (02) DCv2 extended login
+  0x0130019D, // (02) DCv2 extended login (UDP off)
   // Note: PSO PC initial commands are not listed here because we don't use a
   // detector encryption for PSO PC (instead, we use the split reconnect command
   // to send PC to a different port).
