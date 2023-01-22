37c8491dc37ee2ec4447ca9113167bfb4902807c
Martin Michelsen <fuzziqersoftware+g@gmail.com>
2023-01-22 15:32:46 -0800

    fix test-compression paths for github actions env

--- test-compression.sh
+++ test-compression.sh
@@ -2,24 +2,27 @@
 
 set -e
 
-make
+EXECUTABLE="$1"
+if [ "$EXECUTABLE" == "" ]; then
+  EXECUTABLE="./newserv"
+fi
 
 echo "... decompress card definitions"
-./newserv decompress-prs system/ep3/card-definitions.mnr card-defs.mnrd
+$EXECUTABLE decompress-prs system/ep3/card-definitions.mnr card-defs.mnrd
 echo "... compress card definitions"
-./newserv compress-prs card-defs.mnrd card-defs.mnr
+$EXECUTABLE compress-prs card-defs.mnrd card-defs.mnr
 echo "... check compressed card definitions"
-./newserv decompress-prs card-defs.mnr - | diff card-defs.mnrd -
+$EXECUTABLE decompress-prs card-defs.mnr - | diff card-defs.mnrd -
 
 echo "... recompress executable with PRS"
-./newserv compress-prs newserv newserv.prs
-./newserv decompress-prs newserv.prs
-diff newserv newserv.prs.dec
+$EXECUTABLE compress-prs $EXECUTABLE newserv.prs
+$EXECUTABLE decompress-prs newserv.prs
+diff $EXECUTABLE newserv.prs.dec
 
 echo "... recompress executable with BC0"
-./newserv compress-bc0 newserv newserv.bc0
-./newserv decompress-bc0 newserv.bc0
-diff newserv newserv.bc0.dec
+$EXECUTABLE compress-bc0 $EXECUTABLE newserv.bc0
+$EXECUTABLE decompress-bc0 newserv.bc0
+diff $EXECUTABLE newserv.bc0.dec
 
 echo "... clean up"
 rm card-defs.mnrd card-defs.mnr newserv.prs newserv.prs.dec newserv.bc0 newserv.bc0.dec
