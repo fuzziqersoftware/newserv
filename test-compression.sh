a3249ab19b7407627eca716e19eac5cb1bbfaec4
Martin Michelsen <fuzziqersoftware+g@gmail.com>
2023-03-08 10:48:22 -0800

    split compression tests and skip executable recompression

--- test-compression.sh
+++ test-compression.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-EXECUTABLE="$1"
+SCHEME=$1
+
+EXECUTABLE="$2"
 if [ "$EXECUTABLE" == "" ]; then
   EXECUTABLE="./newserv"
 fi
@@ -10,19 +12,9 @@ fi
 echo "... decompress card definitions"
 $EXECUTABLE decompress-prs system/ep3/card-definitions.mnr card-defs.mnrd
 echo "... compress card definitions"
-$EXECUTABLE compress-prs card-defs.mnrd card-defs.mnr
+$EXECUTABLE compress-$SCHEME card-defs.mnrd card-defs.mnr.$SCHEME
 echo "... check compressed card definitions"
-$EXECUTABLE decompress-prs card-defs.mnr - | diff card-defs.mnrd -
-
-echo "... recompress executable with PRS"
-$EXECUTABLE compress-prs $EXECUTABLE newserv.prs
-$EXECUTABLE decompress-prs newserv.prs
-diff $EXECUTABLE newserv.prs.dec
-
-echo "... recompress executable with BC0"
-$EXECUTABLE compress-bc0 $EXECUTABLE newserv.bc0
-$EXECUTABLE decompress-bc0 newserv.bc0
-diff $EXECUTABLE newserv.bc0.dec
+$EXECUTABLE decompress-$SCHEME card-defs.mnr.$SCHEME - | diff card-defs.mnrd -
 
 echo "... clean up"
-rm card-defs.mnrd card-defs.mnr newserv.prs newserv.prs.dec newserv.bc0 newserv.bc0.dec
+rm card-defs.mnrd card-defs.mnr.$SCHEME
