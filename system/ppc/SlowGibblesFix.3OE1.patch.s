341413170cc6fda49f41bf0789bbdaf82816a25b
Jake Probst <jake.probst@gmail.com>
2023-06-07 20:18:48 -0600

    I don't even know what was going on here

--- system/ppc/SlowGibblesFix.3OE1.patch.s
+++ system/ppc/SlowGibblesFix.3OE1.patch.s
@@ -4,10 +4,10 @@ reloc0:
 
 start:
     mflr r7
-    lis r4, 0x804D
-    ori r4, r4, 0xE278
-    li r5, 0x3f33
-    ori r5, r5, 0x3333
+    lis r1, 0x804D
+    ori r1, r1, 0xE278
+    li r2, 0x3f33
+    ori r2, r2, 0x3333
     stw [r1], r2
     mtlr r7
     blr
