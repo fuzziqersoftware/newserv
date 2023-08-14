102fe92c3aa364e281c2ca62c45b07dbcbb54eef
Martin Michelsen <fuzziqersoftware+g@gmail.com>
2023-08-14 11:32:54 -0700

    add note about Ep3 trial B8 command

--- src/CommandFormats.hh
+++ src/CommandFormats.hh
@@ -2220,7 +2220,8 @@ struct S_RankUpdate_GC_Ep3_B7 {
 // data, followed immediately by the data. The maximum size of the compressed
 // data is 0x9000 bytes, although the receive buffer size limit applies first in
 // practice, which limits this to 0x7BF8 bytes. The maximum size of the
-// decompressed data is 0x36EC0 bytes.
+// decompressed data is 0x36EC0 bytes on retail Episode 3, or 0x32960 bytes on
+// Trial Edition.
 // Note: PSO BB accepts this command as well, but ignores it.
 
 // B8 (C->S): Confirm updated card definitions (Episode 3)
