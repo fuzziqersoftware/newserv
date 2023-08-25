1d0c0088d608487563f6f5c4e6a2dbe8151a982e
Martin Michelsen <fuzziqersoftware+g@gmail.com>
2023-08-24 23:16:59 -0700

    add notes from GameJam demo

--- src/Version.cc
+++ src/Version.cc
@@ -62,7 +62,7 @@ uint32_t flags_for_version(GameVersion version, int64_t sub_version) {
           Client::Flag::SEND_FUNCTION_CALL_CHECKSUM_ONLY |
           Client::Flag::SEND_FUNCTION_CALL_NO_CACHE_PATCH;
 
-    case 0x30: // GC Ep1&2 JP v1.02, at least one version of PSO XB
+    case 0x30: // GC Ep1&2 GameJam demo, GC Ep1&2 JP v1.02, at least one version of PSO XB
     case 0x31: // GC Ep1&2 US v1.00, GC US v1.01, GC EU v1.00, GC JP v1.00
     case 0x34: // GC Ep1&2 JP v1.03
       // In the case of GC Trial Edition, the IS_GC_TRIAL_EDITION flag is
