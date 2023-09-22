aa25f7e79a7f87ba8cebf08b0793918299e5a650
Martin Michelsen <fuzziqersoftware+g@gmail.com>
2023-09-21 17:08:18 -0700

    make compression tests not fail when run in parallel

--- tests/test-compression.sh
+++ tests/test-compression.sh
@@ -9,36 +9,37 @@ if [ "$EXECUTABLE" == "" ]; then
   EXECUTABLE="./newserv"
 fi
 
+BASENAME="card-defs-test-$SCHEME"
 
 echo "... decompress"
-$EXECUTABLE decompress-prs system/ep3/card-definitions.mnr card-defs.mnrd
+$EXECUTABLE decompress-prs system/ep3/card-definitions.mnr $BASENAME.mnrd
 
 echo "... compress with level=-1 (no compression)"
-$EXECUTABLE compress-$SCHEME --compression-level=-1 card-defs.mnrd card-defs.mnrd.$SCHEME.lN
+$EXECUTABLE compress-$SCHEME --compression-level=-1 $BASENAME.mnrd $BASENAME.mnrd.$SCHEME.lN
 echo "... compress with level=0"
-$EXECUTABLE compress-$SCHEME --compression-level=0 card-defs.mnrd card-defs.mnrd.$SCHEME.l0
+$EXECUTABLE compress-$SCHEME --compression-level=0 $BASENAME.mnrd $BASENAME.mnrd.$SCHEME.l0
 echo "... compress with level=1"
-$EXECUTABLE compress-$SCHEME --compression-level=1 card-defs.mnrd card-defs.mnrd.$SCHEME.l1
+$EXECUTABLE compress-$SCHEME --compression-level=1 $BASENAME.mnrd $BASENAME.mnrd.$SCHEME.l1
 
 echo "... decompress from level=-1 (no compression)"
-$EXECUTABLE decompress-$SCHEME card-defs.mnrd.$SCHEME.lN card-defs.mnrd.$SCHEME.lN.dec
+$EXECUTABLE decompress-$SCHEME $BASENAME.mnrd.$SCHEME.lN $BASENAME.mnrd.$SCHEME.lN.dec
 echo "... decompress from level=0"
-$EXECUTABLE decompress-$SCHEME card-defs.mnrd.$SCHEME.l0 card-defs.mnrd.$SCHEME.l0.dec
+$EXECUTABLE decompress-$SCHEME $BASENAME.mnrd.$SCHEME.l0 $BASENAME.mnrd.$SCHEME.l0.dec
 echo "... decompress from level=1"
-$EXECUTABLE decompress-$SCHEME card-defs.mnrd.$SCHEME.l1 card-defs.mnrd.$SCHEME.l1.dec
+$EXECUTABLE decompress-$SCHEME $BASENAME.mnrd.$SCHEME.l1 $BASENAME.mnrd.$SCHEME.l1.dec
 
 echo "... check result from level=-1 (no compression)"
-diff card-defs.mnrd card-defs.mnrd.$SCHEME.lN.dec
+diff $BASENAME.mnrd $BASENAME.mnrd.$SCHEME.lN.dec
 echo "... check result from level=0"
-diff card-defs.mnrd card-defs.mnrd.$SCHEME.l0.dec
+diff $BASENAME.mnrd $BASENAME.mnrd.$SCHEME.l0.dec
 echo "... check result from level=1"
-diff card-defs.mnrd card-defs.mnrd.$SCHEME.l1.dec
+diff $BASENAME.mnrd $BASENAME.mnrd.$SCHEME.l1.dec
 
 echo "... clean up"
-rm card-defs.mnrd \
-    card-defs.mnrd.$SCHEME.lN \
-    card-defs.mnrd.$SCHEME.l0 \
-    card-defs.mnrd.$SCHEME.l1 \
-    card-defs.mnrd.$SCHEME.lN.dec \
-    card-defs.mnrd.$SCHEME.l0.dec \
-    card-defs.mnrd.$SCHEME.l1.dec
+rm $BASENAME.mnrd \
+    $BASENAME.mnrd.$SCHEME.lN \
+    $BASENAME.mnrd.$SCHEME.l0 \
+    $BASENAME.mnrd.$SCHEME.l1 \
+    $BASENAME.mnrd.$SCHEME.lN.dec \
+    $BASENAME.mnrd.$SCHEME.l0.dec \
+    $BASENAME.mnrd.$SCHEME.l1.dec
