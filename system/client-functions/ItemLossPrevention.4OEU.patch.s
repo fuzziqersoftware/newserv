b1f419e3372c521e5ce7c494b9bd747b160e07a7
Martin Michelsen <fuzziqersoftware+g@gmail.com>
2024-02-10 00:44:40 -0800

    use a more concise format for xb patches

--- system/client-functions/ItemLossPrevention.4OEU.patch.s
+++ system/client-functions/ItemLossPrevention.4OEU.patch.s
@@ -1,4 +1,3 @@
-
 .meta name="No item loss"
 .meta description="Don't lose items if\nyou don't log off\nnormally"
 # Original code by Ralf @ GC-Forever and Aleron Ives
@@ -10,59 +9,22 @@ reloc0:
   .offsetof start
 start:
   .include  WriteCodeBlocksXB
-  .data     0x000D1BD5
-  .data     0x00000001
-  .binary   00
-  .data     0x000D1C4C
-  .data     0x00000002
-  .binary   EB08
-  .data     0x0020E805
-  .data     0x00000001
-  .binary   EB
-  .data     0x00211A2A
-  .data     0x00000002
-  .binary   EB74
-  .data     0x00229255
-  .data     0x00000002
-  .binary   9090
-  .data     0x002292D7
-  .data     0x00000002
-  .binary   EB08
-  .data     0x0022A2C2
-  .data     0x00000002
-  .binary   9090
-  .data     0x0022A33B
-  .data     0x00000002
-  .binary   EB08
-  .data     0x0022BFD5
-  .data     0x00000001
-  .binary   00
-  .data     0x0022C00E
-  .data     0x00000002
-  .binary   EB08
-  .data     0x0022C386
-  .data     0x00000001
-  .binary   00
-  .data     0x00241A78
-  .data     0x00000001
-  .binary   00
-  .data     0x00241AEC
-  .data     0x00000002
-  .binary   EB08
-  .data     0x002A2B34
-  .data     0x00000001
-  .binary   00
-  .data     0x002A2BAC
-  .data     0x00000002
-  .binary   EB08
-  .data     0x002D6C8A
-  .data     0x00000001
-  .binary   00
-  .data     0x002D6CFD
-  .data     0x00000002
-  .binary   EB08
-  .data     0x002F0FCE
-  .data     0x00000001
-  .binary   EB
-  .data     0x00000000
-  .data     0x00000000
+  .binary   000D1BD5 00000001 00
+  .binary   000D1C4C 00000002 EB08
+  .binary   0020E805 00000001 EB
+  .binary   00211A2A 00000002 EB74
+  .binary   00229255 00000002 9090
+  .binary   002292D7 00000002 EB08
+  .binary   0022A2C2 00000002 9090
+  .binary   0022A33B 00000002 EB08
+  .binary   0022BFD5 00000001 00
+  .binary   0022C00E 00000002 EB08
+  .binary   0022C386 00000001 00
+  .binary   00241A78 00000001 00
+  .binary   00241AEC 00000002 EB08
+  .binary   002A2B34 00000001 00
+  .binary   002A2BAC 00000002 EB08
+  .binary   002D6C8A 00000001 00
+  .binary   002D6CFD 00000002 EB08
+  .binary   002F0FCE 00000001 EB
+  .binary   00000000 00000000
