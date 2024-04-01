95919b8b01f63ddeb0435ff8ac7b3399bdc765ae
Martin Michelsen <fuzziqersoftware+g@gmail.com>
2024-03-31 17:59:18 -0700

    add xbox hungry mag sound patch

---
+++ system/client-functions/HungryMagSound.4OJU.patch.s
@@ -0,0 +1,27 @@
+.meta name="MAG alert"
+.meta description="Play a sound when\nyour MAG is hungry"
+# Original code by Ralf @ GC-Forever and Aleron Ives
+# https://www.gc-forever.com/forums/viewtopic.php?t=2050
+# https://www.gc-forever.com/forums/viewtopic.php?t=2049
+# Xbox port by fuzziqersoftware
+
+entry_ptr:
+reloc0:
+  .offsetof start
+start:
+  .include  WriteCodeBlocksXB
+
+  .data     0x00181125
+  .data     0x0000000A
+  .binary   E998010000CCCC83C410
+
+  .data     0x001812C2
+  .data     0x0000000D
+  .binary   31C0898694010000505050EB52
+
+  .data     0x00181321
+  .data     0x0000000F
+  .binary   048D50BA90B32E00FFD2E9FCFDFFFF
+
+  .data     0x00000000
+  .data     0x00000000
