9d2b36b787ab2239c9b1bc70fde08f9d7fa978e1
Martin Michelsen <fuzziqersoftware+g@gmail.com>
2024-04-01 21:49:40 -0700

    add idle disconnect patch

---
+++ system/client-functions/DisableIdleDisconnect.3OJ3.patch.s
@@ -0,0 +1,15 @@
+.meta name="Disable idle DC"
+.meta description="Disable idle\ndisconnect timeout"
+
+entry_ptr:
+reloc0:
+  .offsetof start
+start:
+  .include  WriteCodeBlocksGC
+
+  .data     0x80134FA0
+  .data     0x00000004
+  .data     0x38600000
+
+  .data     0x00000000
+  .data     0x00000000
---
+++ system/client-functions/DisableIdleDisconnect.4OJD.patch.s
@@ -0,0 +1,15 @@
+.meta name="Disable idle DC"
+.meta description="Disable idle\ndisconnect timeout"
+
+entry_ptr:
+reloc0:
+  .offsetof start
+start:
+  .include  WriteCodeBlocksXB
+
+  .data     0x002C167E
+  .data     0x00000004
+  .binary   31C9EB03
+
+  .data     0x00000000
+  .data     0x00000000
