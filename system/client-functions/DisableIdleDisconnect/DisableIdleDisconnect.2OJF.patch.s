451c8d5e0960da069693217906bc3bd821dc5985
Martin Michelsen <fuzziqersoftware+g@gmail.com>
2024-05-04 11:52:06 -0700

    add DC idle disconnect patch

---
+++ system/client-functions/DisableIdleDisconnect/DisableIdleDisconnect.2OJF.patch.s
@@ -0,0 +1,17 @@
+.meta name="Disable idle DC"
+.meta description="Disable idle\ndisconnect timeout"
+
+entry_ptr:
+reloc0:
+  .offsetof start
+start:
+  .include  WriteCodeBlocksDC
+
+  .align 4
+  .data     0x8C01B684
+  .data     0x00000002
+  mov       r0, 0
+
+  .align 4
+  .data     0x00000000
+  .data     0x00000000
