7ad5cbd28b342e95f82c5ab792861d43031bf130
Blst34 <85366425+Blst34@users.noreply.github.com>
2025-02-13 14:38:11 +0200

    Add files via upload

---
+++ system/client-functions/RaresInQuests/RaresInQuests.2OPF.patch.s
@@ -0,0 +1,22 @@
+.meta name="Rares in quests"
+.meta description="Disables logic that\nprevents items\nabove 8 stars and\nrares from dropping\nin quests."
+
+entry_ptr:
+reloc0:
+  .offsetof start
+start:
+  .include  WriteCodeBlocksDC
+
+  .align    4
+  .data     0x8C219E6C
+  .data     2
+  nop
+
+    .align    4
+  .data     0x8C219DF8
+  .data     2
+  nop
+
+  .align    4
+  .data     0x00000000
+  .data     0x00000000
